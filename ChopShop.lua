16a7863377951f4a98aac5a5e17a9f4e60c74a0f
raphAlreadyTaken <raflegrand@hotmail.com>
2022-10-08 19:28:01 +0200

    fix: Button order

--- ChopShop.lua
+++ ChopShop.lua
@@ -7,8 +7,8 @@ local MICRO_BUTTONS = {
     QuestLogMicroButton,
     GuildMicroButton,
     LFDMicroButton,
-    EJMicroButton,
     CollectionsMicroButton,
+    EJMicroButton,
     MainMenuMicroButton,
     HelpMicroButton,
 }
