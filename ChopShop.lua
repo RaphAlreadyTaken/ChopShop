dbf5737021d391f0e6d2122a13e4dfb98888346f
raphAlreadyTaken <raflegrand@hotmail.com>
2022-10-28 21:37:58 +0200

    fix(DF pre-patch): Unnecessary stuff removed + button bar correctly repositioned + API changes applied

--- ChopShop.lua
+++ ChopShop.lua
@@ -1,49 +1,13 @@
---  List of buttons to process after shop removal
-local MICRO_BUTTONS = {
-    CharacterMicroButton,
-    SpellbookMicroButton,
-    TalentMicroButton,
-    AchievementMicroButton,
-    QuestLogMicroButton,
-    GuildMicroButton,
-    LFDMicroButton,
-    CollectionsMicroButton,
-    EJMicroButton,
-    MainMenuMicroButton,
-    HelpMicroButton,
-}
-
 -- Buttons should be stacked (true) or not
 local stacked = false;
 
-
 ----************ Local functions ************----
 
--- Positions buttons next to one another with an x offset
-function positionButtons(buttonList, xOffset)
-    local currentButton = buttonList[1];
-
-    for i = 2, #buttonList do
-        buttonList[i]:ClearAllPoints();
-        buttonList[i]:SetPoint("BOTTOMLEFT", currentButton, "BOTTOMRIGHT", xOffset, 0);
-        currentButton = buttonList[i]
-    end
+-- Repositions Character button (first button) with offsets
+function reanchorCharacterMicroButton(xOffset, yOffset)
+    CharacterMicroButton:ClearAllPoints();
+    CharacterMicroButton:SetPoint('BOTTOMLEFT', CharacterMicroButton:GetParent(), 'BOTTOMLEFT', xOffset, yOffset);
 end
-
--- Distributes remaining buttons in MicroButtonAndBagsBar (main layout)
-function spaceButtonsOut(buttonList)
-    positionButtons(buttonList, 1);
-end
-
--- Stacks buttons in MicroButtonAndBagsBar (vehicle and pet battle layouts)
-function stackButtons(buttonList)
-    positionButtons(buttonList, -2);
-
-    -- LFDMicroButton is the first button of the second row
-    LFDMicroButton:ClearAllPoints();
-    LFDMicroButton:SetPoint("TOPLEFT", CharacterMicroButton, "BOTTOMLEFT", 0, -1);
-end
-
 
 ----************ Hooks ************----
 
@@ -51,39 +15,40 @@ end
 hooksecurefunc("UpdateMicroButtons", function(...)
     StoreMicroButton:Hide();
 
+    MainMenuMicroButton:ClearAllPoints();
+    MainMenuMicroButton:SetPoint("TOPLEFT", EJMicroButton, "TOPRIGHT", 1, 0);
+
     if not stacked then
-        spaceButtonsOut(MICRO_BUTTONS);
+        reanchorCharacterMicroButton(27, 6);
     end
 end)
 
--- Repositions buttons on layout changes
+-- Repositions buttons on layout changes (enter/exit pet battle or vehicle)
 hooksecurefunc("MoveMicroButtons", function(_, _, _, _, _, isStacked)
     stacked = isStacked;
 
     if stacked then
-        stackButtons(MICRO_BUTTONS);
+        reanchorCharacterMicroButton(9, 26);
     else
-        spaceButtonsOut(MICRO_BUTTONS);
+        reanchorCharacterMicroButton(27, 6);
     end
 end)
 
 -- Removes shop button from escape menu
 hooksecurefunc("GameMenuFrame_UpdateVisibleButtons", function(...)
-    local height = 292;
-    GameMenuButtonUIOptions:SetPoint("TOP", GameMenuButtonOptions, "BOTTOM", 0, -1);
+    local height = 280;
 
     local buttonToReanchor = GameMenuButtonWhatsNew;
     local reanchorYOffset = -1;
 
-    if IsCharacterNewlyBoosted() or not C_SplashScreen.CanViewSplashScreen()  then
+    if IsCharacterNewlyBoosted() or not C_SplashScreen.CanViewSplashScreen() then
         GameMenuButtonWhatsNew:Hide();
         height = height - 20;
-        buttonToReanchor = GameMenuButtonOptions;
+        buttonToReanchor = GameMenuButtonSettings;
         reanchorYOffset = -16;
         GameMenuFrame:SetHeight(height + 16);
     else
         GameMenuButtonWhatsNew:Show();
-        GameMenuButtonOptions:SetPoint("TOP", GameMenuButtonWhatsNew, "BOTTOM", 0, -16);
         GameMenuFrame:SetHeight(height + 18);
     end
 
