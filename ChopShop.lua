bd0199e487afe38247af1302e69951861bbf9f5b
raphAlreadyTaken <raflegrand@hotmail.com>
2024-07-28 11:45:49 +0200

    fix(11.0.0): Disable escape menu button removal

--- ChopShop.lua
+++ ChopShop.lua
@@ -4,25 +4,3 @@
 hooksecurefunc("UpdateMicroButtons", function(...)
     StoreMicroButton:Hide();
 end)
-
--- Removes shop button from escape menu
-hooksecurefunc("GameMenuFrame_UpdateVisibleButtons", function(...)
-    local height = 280;
-
-    local buttonToReanchor = GameMenuButtonWhatsNew;
-    local reanchorYOffset = -1;
-
-    if IsCharacterNewlyBoosted() or not C_SplashScreen.CanViewSplashScreen() then
-        GameMenuButtonWhatsNew:Hide();
-        height = height - 20;
-        buttonToReanchor = GameMenuButtonSettings;
-        reanchorYOffset = -16;
-        GameMenuFrame:SetHeight(height + 16);
-    else
-        GameMenuButtonWhatsNew:Show();
-        GameMenuFrame:SetHeight(height + 18);
-    end
-
-    GameMenuButtonStore:Hide();
-    buttonToReanchor:SetPoint("TOP", GameMenuButtonHelp, "BOTTOM", 0, reanchorYOffset);
-end)
